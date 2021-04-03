6d742571236865b866c9e97f998896818423ad44
Samuel Calabresi <samuel.calabresi@broadbean.com>
2021-04-03 11:03:25 +0200

    updating config.js with cognito ids

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "eu-central-1_u7v2yFw8x", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "7l1de7ai2c36827as2iec9re09", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "eu-central-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
